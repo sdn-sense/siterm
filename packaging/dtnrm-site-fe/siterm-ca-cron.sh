362b34b4c0c95aa37f07198a4671f31481681461
root <root@sdn-dtn-1-7.ultralight.org>
2020-04-20 11:22:04 -0700

    Install Local CAs to all Frontends

---
+++ packaging/dtnrm-site-fe/siterm-ca-cron.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TMP_DIR=$(mktemp -d)
+GIT_REPO=`cat /etc/dtnrm.yaml | grep 'GIT_REPO' | awk '{print $2}' | tr -d '"' | tr -d "'"`
+CA_DIR=/etc/grid-security/certificates
+
+cd $TMP_DIR
+git clone https://github.com/$GIT_REPO .
+
+cd $TMP_DIR/CAs/
+for fname in `ls *.pem`; do
+  cp $fname $CA_DIR/
+  hash=$(openssl x509 -hash -in "$CA_DIR$/$fname" |head -n 1)
+  ln -sf $CA_DIR$/$fname $hash.0
+done
+
+rm -rf $TMP_DIR
