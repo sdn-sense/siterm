f5170c1497800a325ed1bccd5f590432a91b1ea9
Justas Balcas <juztas@gmail.com>
2020-04-27 19:12:29 -0700

    Force re-add interface in case status fails

--- src/python/DTNRMAgent/Ruler/Components/VInterfaces.py
+++ src/python/DTNRMAgent/Ruler/Components/VInterfaces.py
@@ -13,16 +13,17 @@ Copyright 2017 California Institute of Technology
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-Title 			: dtnrm
-Author			: Justas Balcas
-Email 			: justas.balcas (at) cern.ch
-@Copyright		: Copyright (C) 2016 California Institute of Technology
-Date			: 2017/09/26
+Title             : dtnrm
+Author            : Justas Balcas
+Email             : justas.balcas (at) cern.ch
+@Copyright        : Copyright (C) 2016 California Institute of Technology
+Date            : 2017/09/26
 """
 # TODO. Configure also MTU and txqueuelen
 import ipaddress
+import netifaces
 from DTNRMLibs.MainUtilities import execute
-
+from DTNRMLibs.CustomExceptions import FailedInterfaceCommand
 
 def getBroadCast(inIP):
     """ Return broadcast IP """
@@ -33,6 +34,7 @@ def getBroadCast(inIP):
 def identifyL23(addition):
     """ Check if it is L2 or L3 delta request """
     return 'L3' if 'routes' in addition.keys() else 'L2'
+
 
 
 class VInterfaces(object):
@@ -113,21 +115,45 @@ class VInterfaces(object):
 
     def status(self, vlan, raiseError=False):
         """ Get status of specific vlan """
+        out = None
         if identifyL23(vlan) == 'L2':
             self.logger.info('Called VInterface status for %s' % str(vlan))
             command = "ip link show dev vlan.%s" % (vlan['vlan'])
-            return execute(command, self.logger, raiseError)
+            out = execute(command, self.logger, raiseError)
+            self.checkInterfacePresense(vlan, raiseError)
         else:
             self.logger.info('Called VInterface status L3 for %s' % str(vlan))
             for routel in vlan['routes']:
                 if 'routeTo' in routel.keys() and 'nextHop' in routel.keys():
                     if 'value' in routel['routeTo'].keys() and 'value' in routel['nextHop'].keys():
                         command = "ip route get %s" % (routel['routeTo']['value'])
-                        execute(command, self.logger, raiseError)
+                        out = execute(command, self.logger, raiseError)
                 else:
                     self.logger.info('Parsed delta did not had routeTo or nextHop keys in route info. Route details: %s'
                                      % routel)
-        return None
+        return out
+
+    def checkInterfacePresense(self, vlan, raiseError=False):
+        error = None
+        allInterfaces = netifaces.interfaces()
+        if 'vlan.%s' % vlan['vlan'] not in allInterfaces:
+            self.logger.debug('Previously called debug FAILED. Retry: %s' % str(vlan))
+            error = "Interface is not present for %s" % vlan
+        else:
+            allIPs = netifaces.ifaddresses('vlan.%s' % vlan['vlan'])
+            if 2 not in allIPs.keys():
+                error = "IPv4 address metrics are not available"
+            else:
+                ipPresent = False
+                serviceIp = vlan['ip'].split('/')[0]
+                for ipv4m in allIPs[2]:
+                    if serviceIp == ipv4m['addr']:
+                        ipPresent = True
+                if not ipPresent:
+                    error = 'IP Is not Set. Trying to reinitiate.'
+        if raiseError and error:
+            raise FailedInterfaceCommand(error)
+
 
 if __name__ == '__main__':
     print 'This has to be called through main Ruler component. Not supported direct call'
