a383565be3c55d46843cc823d831e36a1f045cba
Justas Balcas <juztas@gmail.com>
2020-09-23 16:23:16 -0700

    Update node exporter docker

--- installers/node-exporter/start.sh
+++ installers/node-exporter/start.sh
@@ -1,6 +1,8 @@
 docker run -d -p 9100:9100 \
+  --net="host" \
+  --pid="host" \
   -v "/proc:/host/proc:ro" \
   -v "/sys:/host/sys:ro" \
-  -v "/:/rootfs:ro" \
-  --net="host" \
+  -v "/:/host:ro,rslave" \
   node_exporter \
+  --path.rootfs=/host
