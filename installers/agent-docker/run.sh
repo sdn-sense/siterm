f23b4418fd80bd6feee7125feb4fa7c07f4acfaa
Justas Balcas <juztas@gmail.com>
2021-10-27 09:30:30 -0700

    use json log driver for docker (#89)
    Co-authored-by: Justas Balcas <jbalcas@sdn-login-1.ultralight.org>

--- installers/agent-docker/run.sh
+++ installers/agent-docker/run.sh
@@ -6,5 +6,5 @@ docker run \
        -v $(pwd)/conf/opt/config/:/opt/config/ \
        --cap-add=NET_ADMIN \
        --net=host \
-       --log-opt max-size=10m --log-opt max-file=10 \
+       --log-driver="json-file" --log-opt max-size=10m --log-opt max-file=10 \
        siteagent
