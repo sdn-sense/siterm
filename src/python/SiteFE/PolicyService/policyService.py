cba8a624f45aa9a2265c23bb5cc3d10e6a31c6b6
Justas Balcas <juztas@gmail.com>
2022-07-25 08:40:21 -0700

    mend

--- src/python/SiteFE/PolicyService/policyService.py
+++ src/python/SiteFE/PolicyService/policyService.py
@@ -388,8 +388,6 @@ class PolicyService(RDFHelper):
         self.logger.info('Conflict Check of expired entities')
         newconf, cleaned = self.conflictChecker.checkActiveConfig(currentActive['output'])
         self.logger.info('Conflict Check for diff outcomes')
-        import pprint
-        pprint.pprint(currentActive['output'])
         if cleaned or self.conflictChecker.checkConflicts(self, newconf, currentActive['output']):
             self.logger.info('IMPORTANT: State changed. Writing new config to DB.')
             currentActive['output'] = newconf
