d676b4681d1650e7a94c3a24ab9453d036ca36f5
Justas Balcas <juztas@gmail.com>
2023-12-07 10:57:09 -0600

    linter script (#382)

--- linter.sh
+++ linter.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
-for fname in `git diff --name-only HEAD HEAD~1`; do
-    black $fname;
-    isort $fname;
-    pylint $fname --rcfile standarts/pylintrc
+#!/bin/bash
+for fname in $(git diff --name-only HEAD HEAD~1); do
+    if [[ $fname == *.py ]]
+    then
+        echo "Checking $fname with python linters"
+        black "$fname"
+        isort "$fname"
+        pylint-3 "$fname" --rcfile standarts/pylintrc
+    fi
+    if [[ $fname == *.yaml || $fname == *.yml ]]
+    then
+        echo "Checking $fname with yaml linters"
+        yamllint "$fname"
+    fi
+    if [[ $fname == *.sh ]]
+    then
+        echo "Checking $fname with bash linter"
+        bashlint "$fname"
+    fi
 done
