0717d4d2c6419530e78d3c66570337a1ae18405c
Justas Balcas <juztas@gmail.com>
2024-03-06 09:42:11 -0600

    Addtags for ip uri; Add DB Cleaner and delete older than a week entries (#468)
    * Add tags to ip entries
    
    * Add DBCleaner process

--- setup-sitefe.py
+++ setup-sitefe.py
@@ -13,13 +13,15 @@ from setupUtilities import list_packages, get_py_modules, get_web_files, VERSION
 # Also another cronjob, which monitors config file and modifies cronjobs if needed.
 # Currently it is allowed to specify only minutes and up to 30 minutes.
 # This is how CRONJOBS are handled and division is done only for the current hour.
-SCRIPTS = ["packaging/general/Config-Fetcher", "packaging/general/siterm-debugger", "packaging/general/siterm-bgprocess"]
+SCRIPTS = ["packaging/general/Config-Fetcher",
+           "packaging/general/siterm-debugger",
+           "packaging/general/siterm-bgprocess"]
 for sectionName in ['LookUpService', 'SNMPMonitoring']:
-    SCRIPTS.append('packaging/siterm-site-fe/scripts/%s-update' % sectionName)
+    SCRIPTS.append(f'packaging/siterm-site-fe/scripts{sectionName}-update')
 
 setup(
     name='SiteRMSiteFE',
-    version="%s" % VERSION,
+    version=f"{VERSION}",
     long_description="End Site-RM Site installation",
     author="Justas Balcas",
     author_email="juztas@gmail.com",
@@ -33,6 +35,7 @@ setup(
                 ("/etc/httpd/conf.d/", ["packaging/siterm-site-fe/sitefe-httpd.conf",
                                         "packaging/siterm-site-fe/welcome.conf"]),
                 ("/etc/cron.d/", ["packaging/general/siterm-crons"]),
+                ("/usr/local/sbin/", ["packaging/siterm-site-fe/scripts/DBCleaner.py"]),
                 ("/etc/cron-scripts/", ["packaging/general/siterm-ca-cron.sh"])] + get_web_files(),
     py_modules=get_py_modules(['src/python/SiteFE/', 'src/python/SiteRMLibs']),
     scripts=SCRIPTS
