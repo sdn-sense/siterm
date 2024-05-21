efb0785d11bdb900ea55651fde13fa84f0a7a3fe
Justas Balcas <juztas@gmail.com>
2024-05-21 10:24:51 -0500

    Report Host Arp info to FE. FE publish in prometheus output (#520)

---
+++ src/python/SiteRMAgent/RecurringActions/Plugins/ArpInfo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+ArpInfo Plugin to report arp information to FE
+"""
+import pprint
+
+from SiteRMLibs.MainUtilities import getLoggingObject
+from SiteRMLibs.MainUtilities import getArpVals
+from SiteRMLibs.GitConfig import getGitConfig
+
+
+class ArpInfo:
+    """ArpInfo Plugin"""
+
+    def __init__(self, config=None, logger=None):
+        self.config = config if config else getGitConfig()
+        self.logger = logger if logger else getLoggingObject(config=self.config, service="Agent")
+
+
+    def get(self, **_kwargs):
+        """Get lscpu information"""
+        out = {}
+        out.setdefault("arpinfo", [])
+        for item in getArpVals():
+            out["arpinfo"].append(item)
+        return out
+
+
+if __name__ == "__main__":
+    obj = ArpInfo()
+    PRETTY = pprint.PrettyPrinter(indent=4)
+    PRETTY.pprint(obj.get())
