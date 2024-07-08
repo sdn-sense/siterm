e663065d3e6a6440fc837ec109354a422767c74d
Justas Balcas <juztas@gmail.com>
2024-07-08 07:33:08 -0500

    DBCleaner service

---
+++ src/python/SiteRMLibs/DBCleaner.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Database cleaner"""
+import mariadb
+from SiteRMLibs.MainUtilities import (getDBConn, getLoggingObject, getVal, getUTCnow)
+from SiteRMLibs.GitConfig import getGitConfig
+
+
+class DBCleaner:
+    """DBCleaner class"""
+    def __init__(self, config, sitename):
+        self.config = config
+        self.sitename = sitename
+        self.logger = getLoggingObject(config=self.config, service="DBCleaner")
+        self.dbI = getVal(getDBConn("DBWorker", self), **{"sitename": self.sitename})
+        self.nextRun = int(getUTCnow() - 1) # Make sure runs first time
+
+    def clean(self, dbtable, olderthan):
+        """Clean the database"""
+        print(f"Cleaning {dbtable} for {self.sitename}")
+        try:
+            data = self.dbI.get(dbtable, limit=100, orderby=["insertdate", "ASC"])
+        except mariadb.OperationalError:
+            data = self.dbI.get(dbtable, limit=100, orderby=["updatedate", "ASC"])
+        for item in data:
+            if 'updatedate' in item:
+                if item['updatedate'] < int(getUTCnow() - olderthan):
+                    print(f"Deleting {item['id']} from {dbtable}")
+                    self.dbI.delete(dbtable, [["id", item["id"]]])
+            elif 'insertdate' in item:
+                if item['insertdate'] < int(getUTCnow() - olderthan):
+                    print(f"Deleting {item['id']} from {dbtable}")
+                    self.dbI.delete(dbtable, [["id", item["id"]]])
+            else:
+                print(f"Item {item['id']} from {dbtable} does not have timestamp. Ignoring")
+
+    def startwork(self):
+        """Start the cleaner"""
+        if self.nextRun > getUTCnow():
+            return
+        self.nextRun = int(getUTCnow() + 7200) # Run every 2 hours
+        print('Starting cleaner')
+        for table in ['debugrequests', 'deltas', 'deltatimestates', 'hosts', 'models', 'servicestates', 'states',
+                      'hoststates', 'hoststateshistory', 'parsed', 'switch', 'snmpmon', 'serviceaction', 'activeDeltas']:
+            print(f"Cleaning {table}")
+            self.clean(table, 86400*7)
+        print('Cleaner finished')
+
+
+if __name__ == "__main__":
+    logObj = getLoggingObject(logType='StreamLogger', service='DBCleaner')
+    gconfig = getGitConfig()
+    for siteName in gconfig.get("general", "sites"):
+        dbworker = DBCleaner(gconfig, siteName)
+        dbworker.startwork()
