11e26d8ea12c86c3f7b9a4209d1571ea09accc9e
Justas Balcas <juztas@gmail.com>
2024-07-19 09:41:40 -0500

    Improve html reporting alerts. Add Traceroute from host. Fix IPv6 reporting by agent (#598)

--- src/python/SiteRMAgent/Debugger/BackgroundRuns/Traceroute.py
+++ src/python/SiteRMAgent/Debugger/BackgroundRuns/Traceroute.py
@@ -4,10 +4,13 @@ Title                   : siterm
 Author                  : Justas Balcas
 Email                   : juztas (at) gmail (dot) com
 @Copyright              : Copyright (C) 2024 Justas Balcas
-Date                    : 2024/02/26
+Date                    : 2024/07/19
 """
 from SiteRMLibs.BaseDebugAction import BaseDebugAction
-from SiteRMLibs.CustomExceptions import BackgroundException
+from SiteRMLibs.MainUtilities import externalCommandStdOutErr
+from SiteRMLibs.ipaddr import getInterfaces
+from SiteRMLibs.ipaddr import ipVersion
+
 
 class Traceroute(BaseDebugAction):
     """Traceroute class. Run trace route to specific IP."""
@@ -21,6 +24,23 @@ class Traceroute(BaseDebugAction):
 
     def main(self):
         """Main TraceRoute work. Run TraceRoute on host."""
-        self.logMessage(f"NOT IMPLEMENTED call {self.backgConfig} to run traceroute", "warning")
-        self.jsonout['traceroute']['exitCode'] = -1 # Once implemented do 0
-        raise BackgroundException("NOT IMPLEMENTED! -1")
+        cmd = "traceroute "
+        # Add from interface section
+        if 'from_interface' in self.requestdict and self.requestdict['from_interface']:
+            if self.requestdict['from_interface'] not in getInterfaces():
+                self.logMessage(f"Interface {self.requestdict['from_interface']} is not available on the node")
+                return
+            cmd += f"-i {self.requestdict['from_interface']} "
+        # Add from ip section
+        if 'from_ip' in self.requestdict and self.requestdict['from_ip']:
+            if ipVersion(self.requestdict['from_ip']) == -1:
+                self.logMessage(f"IP {self.requestdict['from_ip']} does not appear to be an IPv4 or IPv6")
+                return
+            cmd += f"-s {self.requestdict['from_ip']} "
+        if ipVersion(self.requestdict['ip']) == -1:
+            self.logMessage(f"IP {self.requestdict['ip']} does not appear to be an IPv4 or IPv6")
+            return
+        cmd += f"{self.requestdict['ip']}"
+        self.logMessage(f"Running command: {cmd}")
+        externalCommandStdOutErr(cmd, self.outfiles['stdout'], self.outfiles['stderr'])
+        self.jsonout['exitCode'] = 0
