8ff48b55233270dd9f4b3a8707118cec5c373925
Justas Balcas <juztas@gmail.com>
2025-03-28 09:53:41 -0500

    1.5.23 release

--- setupUtilities.py
+++ setupUtilities.py
@@ -26,7 +26,7 @@ import sys
 # src/python/SiteFE/__init__.py
 # src/python/SiteRMAgent/__init__.py
 # src/python/SiteRMLibs/__init__.py
-VERSION = '1.5.22'
+VERSION = '1.5.23'
 
 
 def get_path_to_root(appendLocation=None):
