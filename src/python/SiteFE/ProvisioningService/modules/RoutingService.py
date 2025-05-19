da8343f5df707de6133811ffc44574398ce30e72
Justas Balcas <juztas@gmail.com>
2025-05-19 12:04:48 -0500

    Check if rst apply not empty. not delete groupid (#855)
    * Check if rst apply not empty. not delete groupid
    
    * Pyink

--- src/python/SiteFE/ProvisioningService/modules/RoutingService.py
+++ src/python/SiteFE/ProvisioningService/modules/RoutingService.py
@@ -22,11 +22,12 @@ UpdateDate              : 2022/05/09
 from SiteRMLibs.MainUtilities import generateMD5
 from SiteRMLibs.ipaddr import normalizedip
 
+
 def dictCompare(inDict, oldDict):
     """Compare dict and set any remaining items
-       from current ansible yaml as absent in new one if
-       it's status is present.
-       Return True if there is a difference, False if not"""
+    from current ansible yaml as absent in new one if
+    it's status is present.
+    Return True if there is a difference, False if not"""
     # If equal - return
     eqval = False
     if inDict == oldDict:
@@ -38,105 +39,147 @@ def dictCompare(inDict, oldDict):
                 # if it is empty after back from loop, delete
                 del inDict[key]
             # If val has state and it is absent, delete inDictKey also
-            if 'state' in val and val['state'] == 'absent':
+            if "state" in val and val["state"] == "absent":
                 # Means everything already below is absent
                 del inDict[key]
             continue
-        if val == 'present' and key not in inDict.keys():
+        if val == "present" and key not in inDict.keys():
             # Means current state is present, but model does not know anything
-            inDict[key] = 'absent'
+            inDict[key] = "absent"
             eqval = True
-        elif val not in ['present', 'absent']:
+        elif val not in ["present", "absent"]:
             # Ensure we pre-keep all other keys
             inDict[key] = val
     return eqval
 
-class RoutingService():
-    """ Routing Service Class. Adds all activeDelta params,
+
+class RoutingService:
+    """Routing Service Class. Adds all activeDelta params,
     compares with running active config, make things absent if they
     are not existing anymore"""
+
     # pylint: disable=E1101,W0201,W0235
     def __init__(self):
         super().__init__()
 
     @staticmethod
-    def generateGroupName(line):
+    def generateGroupName(activeInfo, line):
         """Generate group name for BGP"""
+        # In case all the rest parameters are empty, we set DEFAULT group name;
+        if all(
+            not activeInfo.get(k)
+            for k in ("ipv6_network", "neighbor", "prefix_list", "route_map")
+        ):
+            # This should never reach, but just in case not to delete any provisioned ones.
+            return "DEFAULT"
         try:
             groupName = line.split("table+")[1].split(":")[0]
             return groupName
         except Exception as err:
-            raise Exception(f'Error while generating group name: {err}') from err
+            raise Exception(f"Error while generating group name: {err}") from err
 
     def _getDefaultBGP(self, host, emptydict=False):
         """Default yaml dict setup"""
         if emptydict:
             tmpD = {}
         else:
-            tmpD = self.yamlconfuuid.setdefault(self.acttype, {}).setdefault(self.connID, {})
+            tmpD = self.yamlconfuuid.setdefault(self.acttype, {}).setdefault(
+                self.connID, {}
+            )
             tmpD = tmpD.setdefault(host, {})
-            tmpD = tmpD.setdefault('sense_bgp', {})
-        tmpD['asn'] = self.getConfigValue(host, 'private_asn')
-        tmpD['belongsTo'] = self.connID
-        if not tmpD['asn']:
-            del tmpD['asn']
-        tmpD['vrf'] = self.getConfigValue(host, 'vrf')
-        if not tmpD['vrf']:
-            del tmpD['vrf']
+            tmpD = tmpD.setdefault("sense_bgp", {})
+        tmpD["asn"] = self.getConfigValue(host, "private_asn")
+        tmpD["belongsTo"] = self.connID
+        if not tmpD["asn"]:
+            del tmpD["asn"]
+        tmpD["vrf"] = self.getConfigValue(host, "vrf")
+        if not tmpD["vrf"]:
+            del tmpD["vrf"]
         if tmpD:
-            tmpD['state'] = 'present'
+            tmpD["state"] = "present"
         return tmpD
 
     def _addOwnRoutes(self, host, rDict):
         """Add Routes"""
         bgpdict = self._getDefaultBGP(host)
-        for iptype in ['ipv4', 'ipv6']:
-            val = rDict.get('routeFrom', {}).get(f'{iptype}-prefix-list', {}).get('value', None)
+        for iptype in ["ipv4", "ipv6"]:
+            val = (
+                rDict.get("routeFrom", {})
+                .get(f"{iptype}-prefix-list", {})
+                .get("value", None)
+            )
             if val:
-                bgpdict.setdefault(f'{iptype}_network', {})
-                bgpdict[f'{iptype}_network'][val] =  'present'
+                bgpdict.setdefault(f"{iptype}_network", {})
+                bgpdict[f"{iptype}_network"][val] = "present"
 
     def _addNeighbors(self, host, ruid, rDict):
         """Add Neighbors"""
         bgpdict = self._getDefaultBGP(host)
-        for iptype in ['ipv4', 'ipv6']:
-            remasn = rDict.get('routeTo', {}).get('bgp-private-asn', {}).get('value', None)
-            remip = rDict.get('nextHop', {}).get(f'{iptype}-address', {}).get('value', None)
+        for iptype in ["ipv4", "ipv6"]:
+            remasn = (
+                rDict.get("routeTo", {}).get("bgp-private-asn", {}).get("value", None)
+            )
+            remip = (
+                rDict.get("nextHop", {}).get(f"{iptype}-address", {}).get("value", None)
+            )
             if remasn and remip:
                 remip = normalizedip(remip)
-                neighbor = bgpdict.setdefault('neighbor', {}).setdefault(iptype, {}).setdefault(remip, {})
+                neighbor = (
+                    bgpdict.setdefault("neighbor", {})
+                    .setdefault(iptype, {})
+                    .setdefault(remip, {})
+                )
                 if neighbor:
-                    raise Exception('Neighbor already defined. MultiPath neighbors not supported')
-                neighbor.setdefault('remote_asn', remasn)
-                neighbor.setdefault('state', 'present')
-                neighbor.setdefault('route_map', {'in': {f'sense-{ruid}-mapin': 'present'},
-                                                  'out': {f'sense-{ruid}-mapout': 'present'}})
+                    raise Exception(
+                        "Neighbor already defined. MultiPath neighbors not supported"
+                    )
+                neighbor.setdefault("remote_asn", remasn)
+                neighbor.setdefault("state", "present")
+                neighbor.setdefault(
+                    "route_map",
+                    {
+                        "in": {f"sense-{ruid}-mapin": "present"},
+                        "out": {f"sense-{ruid}-mapout": "present"},
+                    },
+                )
 
     def _addPrefixList(self, host, ruid, rDict):
         """Add Prefix Lists"""
         bgpdict = self._getDefaultBGP(host)
-        for iptype in ['ipv4', 'ipv6']:
-            rTo = rDict.get('routeFrom', {}).get(f'{iptype}-prefix-list', {}).get('value', None)
-            rFrom = rDict.get('routeTo', {}).get(f'{iptype}-prefix-list', {}).get('value', None)
+        for iptype in ["ipv4", "ipv6"]:
+            rTo = (
+                rDict.get("routeFrom", {})
+                .get(f"{iptype}-prefix-list", {})
+                .get("value", None)
+            )
+            rFrom = (
+                rDict.get("routeTo", {})
+                .get(f"{iptype}-prefix-list", {})
+                .get("value", None)
+            )
             if rTo:
-                prefList = bgpdict.setdefault('prefix_list', {}).setdefault(iptype, {})
+                prefList = bgpdict.setdefault("prefix_list", {}).setdefault(iptype, {})
                 newRoute = prefList.setdefault(rTo, {})
-                newRoute[f'sense-{ruid}-to'] = 'present'
-                self._addRouteMap(host, f'sense-{ruid}-to', f'sense-{ruid}-mapout', iptype)
+                newRoute[f"sense-{ruid}-to"] = "present"
+                self._addRouteMap(
+                    host, f"sense-{ruid}-to", f"sense-{ruid}-mapout", iptype
+                )
             if rFrom:
-                prefList = bgpdict.setdefault('prefix_list', {}).setdefault(iptype, {})
+                prefList = bgpdict.setdefault("prefix_list", {}).setdefault(iptype, {})
                 newRoute = prefList.setdefault(rFrom, {})
-                newRoute[f'sense-{ruid}-from'] = 'present'
-                self._addRouteMap(host, f'sense-{ruid}-from', f'sense-{ruid}-mapin', iptype)
+                newRoute[f"sense-{ruid}-from"] = "present"
+                self._addRouteMap(
+                    host, f"sense-{ruid}-from", f"sense-{ruid}-mapin", iptype
+                )
 
     def _addRouteMap(self, host, match, name, iptype):
         """Add Route Maps"""
         permitst = 10
         bgpdict = self._getDefaultBGP(host)
         if name and match:
-            routeMap = bgpdict.setdefault('route_map', {}).setdefault(iptype, {})
+            routeMap = bgpdict.setdefault("route_map", {}).setdefault(iptype, {})
             permitst += len(routeMap.get(name, {}))
-            routeMap.setdefault(name, {}).setdefault(int(permitst), {match: 'present'})
+            routeMap.setdefault(name, {}).setdefault(int(permitst), {match: "present"})
 
     def _addparamsrst(self, connDict, switches):
         """Wrapper for add params, to put individual request info too inside dictionary"""
@@ -147,7 +190,7 @@ class RoutingService():
             for _, rFullDict in hostDict.items():
                 if not self.checkIfStarted(rFullDict):
                     continue
-                for rtag, rDict in rFullDict.get('hasRoute', {}).items():
+                for rtag, rDict in rFullDict.get("hasRoute", {}).items():
                     ruid = generateMD5(rtag)
                     self._getDefaultBGP(host)
                     self._addOwnRoutes(host, rDict)
@@ -165,36 +208,54 @@ class RoutingService():
 
     def compareBGP(self, switch, runningConf, uuid):
         """Compare L3 BGP"""
-        tmpD = self.yamlconfuuid.setdefault(self.acttype, {}).setdefault(uuid, {}).setdefault(switch, {})
-        tmpD = tmpD.setdefault('sense_bgp', {})
+        tmpD = (
+            self.yamlconfuuid.setdefault(self.acttype, {})
+            .setdefault(uuid, {})
+            .setdefault(switch, {})
+        )
+        tmpD = tmpD.setdefault("sense_bgp", {})
         # If equal - return no difference
         if tmpD == runningConf:
-            self.logger.debug(f'Running conf and new conf are equals for {uuid}. Return False')
+            self.logger.debug(
+                f"Running conf and new conf are equals for {uuid}. Return False"
+            )
             return False
-        self._getDefaultBGP(switch) # Just in case running empty, force add bgp info
+        self._getDefaultBGP(switch)  # Just in case running empty, force add bgp info
         # If runningConf is empty, then it is different
         if not runningConf:
-            self.logger.debug(f'Running config for {uuid} is empty. Return True')
+            self.logger.debug(f"Running config for {uuid} is empty. Return True")
             return True
         different = False
         for key, val in runningConf.items():
             # ipv6_network, ipv4_network, neighbor, prefix_list, route_map
             if not val:
                 continue
-            if key in ['ipv6_network', 'ipv4_network', 'prefix_list', 'route_map', 'neighbor']:
+            if key in [
+                "ipv6_network",
+                "ipv4_network",
+                "prefix_list",
+                "route_map",
+                "neighbor",
+            ]:
                 yamlOut = tmpD.setdefault(key, {})
                 equal = dictCompare(yamlOut, val)
                 if equal:
-                    self.logger.debug(f'There is {key} change for {uuid}. Will return True')
+                    self.logger.debug(
+                        f"There is {key} change for {uuid}. Will return True"
+                    )
                     different = True
             else:
                 tmpD[key] = val
         # Compare empty dict with prepared new conf:
         if self._getDefaultBGP(switch, True) == tmpD:
-            self.logger.debug(f'Def BGP and new BGP is empty for {uuid}. Will return False')
+            self.logger.debug(
+                f"Def BGP and new BGP is empty for {uuid}. Will return False"
+            )
             different = False
         # In case of new - keys not equal, need to write
         if tmpD.keys() != runningConf.keys():
-            self.logger.debug(f'Keys for new BGP are not equal for {uuid}. Will return True')
+            self.logger.debug(
+                f"Keys for new BGP are not equal for {uuid}. Will return True"
+            )
             different = True
         return different
