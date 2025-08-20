6226c249614eae87b3cff7a0f0248e3e22e40962
Justas Balcas <juztas@gmail.com>
2025-08-20 08:53:46 -0500

    Update current version

--- setupUtilities.py
+++ setupUtilities.py
@@ -26,7 +26,7 @@ import sys
 # src/python/SiteFE/__init__.py
 # src/python/SiteRMAgent/__init__.py
 # src/python/SiteRMLibs/__init__.py
-VERSION = '1.5.43'
+VERSION = '1.5.50'
 
 
 def get_path_to_root(appendLocation=None):
