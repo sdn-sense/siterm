d7c94893d216c39d62a9080775fdc369e5d9d23d
Justas Balcas <juztas@gmail.com>
2025-09-11 07:10:34 -0500

    Liveness readiness run max 10 times before breaking

--- src/python/SiteRMLibs/ConfigFetcher.py
+++ src/python/SiteRMLibs/ConfigFetcher.py
@@ -28,6 +28,8 @@ class ConfigFetcher:
         self.gitObj = GitConfig()
         self.config = None
         self.requestHandler = Requests(logger=self.logger)
+        self.failedCounter = "/dev/shm/config-fetcher-counter"
+        self.FetcherReadyFile = "/tmp/config-fetcher-ready"
 
     def refreshthread(self):
         """Call to refresh thread for this specific class and reset parameters"""
@@ -35,11 +37,35 @@ class ConfigFetcher:
         self.config = None
         self.requestHandler.close()
         self.requestHandler = Requests(logger=self.logger)
+        self.failedCounter = "/dev/shm/config-fetcher-counter"
+        self.FetcherReadyFile = "/tmp/config-fetcher-ready"
+
+    def _resetCounter(self):
+        """Reset Counter that informs other services about failures"""
+        with open(self.failedCounter, "w", encoding="utf-8") as fd:
+            fd.write("0")
+
+    def _getCounter(self):
+        """Get Counter of total previous fetch failures"""
+        if os.path.isfile(self.failedCounter):
+            with open(self.failedCounter, "r", encoding="utf-8") as fd:
+                try:
+                    return int(fd.read().strip())
+                except ValueError:
+                    return 0
+        return 0
+
+    def _incrementCounter(self):
+        """Increment Counter that informs other services about failures"""
+        self.logger.info("Incrementing failure counter")
+        currentval = self._getCounter()
+        with open(self.failedCounter, "w", encoding="utf-8") as fd:
+            fd.write(str(currentval + 1))
 
     def _newfetch(self):
         """In case it is a new fetch, remove ready file"""
-        if os.path.isfile("/tmp/config-fetcher-ready"):
-            os.remove("/tmp/config-fetcher-ready")
+        if os.path.isfile(self.FetcherReadyFile):
+            os.remove(self.FetcherReadyFile)
 
     def _fetchFile(self, name, url):
         def retryPolicy(outObj, retries=3):
@@ -125,7 +151,7 @@ class ConfigFetcher:
         # Once removed - reget configs
         self.startwork()
 
-    def startwork(self):
+    def _main(self):
         """Start Config Fetcher Service."""
         self.gitObj.getLocalConfig()
         mapping = self.fetchMapping()
@@ -133,9 +159,28 @@ class ConfigFetcher:
         self.fetchAgent()
         self.fetchFE()
         # Create tmp file that fetcher is done. /tmp/config-fetcher-ready
-        if not os.path.isfile("/tmp/config-fetcher-ready"):
-            with open("/tmp/config-fetcher-ready", "w", encoding="utf-8") as fd:
+        if not os.path.isfile(self.FetcherReadyFile):
+            with open(self.FetcherReadyFile, "w", encoding="utf-8") as fd:
                 fd.write("Ready at: " + str(datetime.datetime.now()))
+
+    def startwork(self):
+        """Start work of Config Fetcher."""
+        count = self._getCounter()
+        if count >= 10:
+            self.logger.error("Got 10 consecutive failures. Will not try to fetch config anymore")
+            self.logger.error("In Kubernetes this will kick the liveness/readiness to be not ready and restart container.")
+            self.logger.error("In docker, this will require manual intervention to restart container.")
+            time.sleep(60)
+            return
+        try:
+            self._main()
+            self._resetCounter()
+        except Exception as ex:
+            self.logger.error(f"Got exception during config fetch: {ex}")
+            self._incrementCounter()
+            count = self._getCounter()
+            if count >= 10:
+                self.logger.error("Got 10 consecutive failures. This will kick the liveness/readiness to be not ready")
 
 
 if __name__ == "__main__":
