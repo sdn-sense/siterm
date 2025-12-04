d670eac335c1f7709de7c5e95908f523f020796d
Justas Balcas <juztas@gmail.com>
2025-12-04 07:48:47 -0600

    Set dev release tag

--- setupUtilities.py
+++ setupUtilities.py
@@ -26,7 +26,7 @@ import sys
 # src/python/SiteFE/__init__.py
 # src/python/SiteRMAgent/__init__.py
 # src/python/SiteRMLibs/__init__.py
-VERSION = '1.5.63'
+VERSION = '1.5.64-dev'
 
 
 def get_path_to_root(appendLocation=None):
