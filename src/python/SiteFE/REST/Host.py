c69b4b6be3e6585077d91e3a23f8b7f23595647f
Justas Balcas <juztas@gmail.com>
2025-12-05 13:42:05 -0600

    Use str for strict boolean default value

--- src/python/SiteFE/REST/Host.py
+++ src/python/SiteFE/REST/Host.py
@@ -131,7 +131,7 @@ async def gethosts(
     request: Request,
     sitename: str = Path(..., description="The site name to retrieve the hosts for.", example=startupConfig.get("SITENAME", "default")),
     hostname: str = Query(None, description="Filter by hostname."),
-    details: StrictBool = Query(False, description="If True, returns detailed host information. In case detail, limit is ignored and set to 1."),
+    details: StrictBool = Query("False", description="If True, returns detailed host information. In case detail, limit is ignored and set to 1."),
     limit: int = Query(LIMIT_DEFAULT, description=f"The maximum number of results to return. Defaults to {LIMIT_DEFAULT}.", ge=LIMIT_MIN, le=LIMIT_MAX),
     deps=Depends(apiReadDeps),
     _forbid=Depends(forbidExtraQueryParams("hostname", "details", "limit")),
